3121d77ac5488f17b0d67824ae4342c764fd1928
Bernard Lambeau <blambeau@gmail.com>
2015-09-25 14:57:20 +0200

    Add src/docemate.js, for a prosemirror packaging for our needs.
    src/docemate.js aims at being a prosemirror packaging (imports)
    for doc-e-mate's needs. It is automatically compiled in the babel
    chain. A `npm run docemate` browserifies it to pkg/prosemirror.js
    through `watchify`

---
+++ src/docemate.js
@@ -0,0 +1,8 @@
+import "./convert/from_markdown"
+import "./convert/to_markdown"
+import "./convert/from_text"
+import "./convert/to_text"
+import "./menu/menubar"
+import "./inputrules/autoinput"
+import {ProseMirror} from "./edit/main"
+module.exports = ProseMirror
